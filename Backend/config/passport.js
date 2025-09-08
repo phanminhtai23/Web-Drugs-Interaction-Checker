03e3a071f7ea969b1ec6c421f8301d6ec49b85c5
MinhTai <phanminhtai23@gmail.com>
2025-09-08 20:16:18 +0700

    chỉnh docker files

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
 const Client = require('../models/client.model');
 
 passport.use(
